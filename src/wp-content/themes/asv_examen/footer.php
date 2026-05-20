f985334737a102c8dc1c063174205fdfc86dce90
AlbertSerra-0802 <aserrav@esliceu.net>
2026-05-20 19:53:37 +0200

    moguennt carpetes

---
+++ src/wp-content/themes/asv_examen/footer.php
@@ -0,0 +1,7 @@
+<footer class="py-4 border-top border-secondary text-center text-white">
+        <p class="small mb-0">© 2025 Apollo Mission. Todos los derechos reservados</p>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
